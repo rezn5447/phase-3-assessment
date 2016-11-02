f6016c3c7fe4cabe709aa44e78fd66fdfad3544a
Resdan Logan <email": null>
2016-11-02 10:05:55 -0700

    part 2 make custom route for home get

--- assessment/config/routes.rb
+++ assessment/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root "pets#index"
 
+  get "home", to: "pets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pets, only: [ :index, :show ]
+  resources :pets, only: [ :index, :show ] do
+    resources :toys, only: :index
+  end
 end
